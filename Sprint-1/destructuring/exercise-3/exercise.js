0681822102bdc76751ce7801eae0225dc7afe3f3
SlimMicheals <slimmicheals77@gmail.com>
2026-02-15 23:41:24 +0100

    Complete destructuring exercise 3 (receipt printing)

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,19 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY  ITEM               TOTAL");
+
+let totalOrderCost = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const itemTotal = (quantity * unitPricePence) / 100;
+  totalOrderCost += itemTotal;
+
+  console.log(
+    `${quantity}    ${itemName.padEnd(18)} ${itemTotal.toFixed(2)}`
+  );
+});
+
+console.log("\nTotal: " + totalOrderCost.toFixed(2));
+
